2b0d13bc331e0ee75bee37348b5340da70bb7b12
Ray Vega <raymond.vega.0@gmail.com>
2019-06-16 12:14:12 -0700

    add test for repl engine execute should run code on continue

--- test/ReplTap.Core.Tests/ReplEngineTests.cs
+++ test/ReplTap.Core.Tests/ReplEngineTests.cs
@@ -21,5 +21,27 @@ namespace ReplTap.Core.Tests
             // assert
             Assert.That(returnValue, Is.EqualTo(expectedReturnValue));
         }
+        
+        
+        [Test]
+        public async Task Execute_Should_Run_Code_On_Continue()
+        {
+            // arrange
+            const string expectedFirstReturnValue = null;
+            const string expectedSecondReturnValue = "test value";
+            
+            var firstCode = $"var testVariable = \"{expectedSecondReturnValue}\";";
+            var secondCode = "testVariable";
+            
+            var engine = new ReplEngine();
+            
+            // act
+            var firstReturnValue  = await engine.Execute(firstCode);
+            var secondReturnValue  = await engine.Execute(secondCode);
+            
+            // assert
+            Assert.That(firstReturnValue, Is.EqualTo(expectedFirstReturnValue));
+            Assert.That(secondReturnValue, Is.EqualTo(expectedSecondReturnValue));
+        }
     }
 }
