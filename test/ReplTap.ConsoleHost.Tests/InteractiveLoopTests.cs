03867b096b4f965fc3f1fca6487f8d5350aad412
Ray Vega <raymond.vega.0@gmail.com>
2019-06-26 21:59:06 -0700

    add test for interactive loop when exception is thrown

--- test/ReplTap.ConsoleHost.Tests/InteractiveLoopTests.cs
+++ test/ReplTap.ConsoleHost.Tests/InteractiveLoopTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
@@ -51,6 +52,49 @@ namespace ReplTap.ConsoleHost.Tests
             // assert
             consoleWriter.Verify(c => c.WriteOutput(output), Times.Once);
         }
-        
+       
+        [Test]
+        public async Task Run_Should_Output_Error_When_Throws_Exception()
+        {
+            // arrange
+            var input = "test code";
+            var errorOutput = "test error output";
+
+            var console = new Mock<IConsole>();
+            var consoleReader = new Mock<IConsoleReader>();
+            var consoleWriter = new Mock<IConsoleWriter>();
+            var replEngine = new Mock<IReplEngine>();
+            var loop = new Mock<ILoop>();
+
+            consoleReader
+                .Setup(c => c.ReadLine(It.IsAny<string>()))
+                .ReturnsAsync(input);
+            
+            replEngine
+                .Setup(r => r.Execute(input))
+                .Throws(new Exception(errorOutput));
+
+            var count = 0;
+            
+            loop
+                .Setup(l => l.Continue())
+                .Returns(() =>
+                {
+                    count++;
+
+                    var shouldContinue = count <= 1;
+                    
+                    return shouldContinue;
+                });
+            
+            var interactiveLoop = new InteractiveLoop(console.Object, 
+                consoleReader.Object, consoleWriter.Object, replEngine.Object, loop.Object);
+            
+            // act && assert
+            await interactiveLoop.Run();
+            
+            // assert
+            consoleWriter.Verify(c => c.WriteError(errorOutput), Times.Once);
+        }
     }
 }
