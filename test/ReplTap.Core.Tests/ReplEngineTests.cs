92f90cfd176ef667350cdf4eb23caca7676b29e5
Ray Vega <raymond.vega.0@gmail.com>
2019-10-10 22:06:14 -0700

    add test when force execute and invalid code in repl engine

--- test/ReplTap.Core.Tests/ReplEngineTests.cs
+++ test/ReplTap.Core.Tests/ReplEngineTests.cs
@@ -10,7 +10,7 @@ namespace ReplTap.Core.Tests
     public class ReplEngineTests
     {
         [Test]
-        public async Task Execute_Should_Run_Code_On_Initial()
+        public async Task Execute_Should_Return_Valid_State_When_Single_Input()
         {
             // arrange
             var expectedOutput = "test output";
@@ -18,73 +18,73 @@ namespace ReplTap.Core.Tests
             var inputCheck = new Mock<IInputCheck>();
 
             var engine = new ReplEngine(inputCheck.Object);
-            
+
             // act
             var result  = await engine.Execute(code);
-            
+
             // assert
             Assert.That(result, Is.Not.Null);
             Assert.That(result.Output, Is.EqualTo(expectedOutput));
             Assert.That(result.State, Is.EqualTo(OutputState.Valid));
         }
-        
+
         [Test]
-        public async Task Execute_Should_Run_Code_On_Continue()
+        public async Task Execute_Should_Return_Valid_State_When_Two_Inputs()
         {
             // arrange
             const string expectedFirstOutput = null;
             const string expectedSecondOutput = "test value";
-            
+
             var firstCode = $"var testVariable = \"{expectedSecondOutput}\";";
             var secondCode = "testVariable";
             var inputCheck = new Mock<IInputCheck>();
-            
+
             var engine = new ReplEngine(inputCheck.Object);
-            
+
             // act
             var firstResult  = await engine.Execute(firstCode);
             var secondResult  = await engine.Execute(secondCode);
-            
+
             // assert
             Assert.That(firstResult.Output, Is.EqualTo(expectedFirstOutput));
             Assert.That(firstResult.State, Is.EqualTo(OutputState.Valid));
-            
+
             Assert.That(secondResult.Output, Is.EqualTo(expectedSecondOutput));
             Assert.That(secondResult.State, Is.EqualTo(OutputState.Valid));
         }
-        
+
         [Test]
-        public async Task Execute_Should_Return_Continue_State_When_Invalid_Code()
+        public async Task Execute_Should_Return_Continue_State_When_Invalid_Input()
         {
             // arrange
             const string expectedFirstResult = null;
             const string expectedSecondResult = "test value";
-            
+
             var firstCode = $"var testVariable = \"{expectedSecondResult}\";";
             var invalidCode = "invalid code;";
             var secondCode = "testVariable";
             var inputCheck = new Mock<IInputCheck>();
-            
+
             var engine = new ReplEngine(inputCheck.Object);
-            
-            // act 
+
+            // act
             var firstResult  = await engine.Execute(firstCode);
             var invalidResult = await engine.Execute(invalidCode);
             var secondResult  = await engine.Execute(secondCode);
-            
+
             // assert
             Assert.That(firstResult.Output, Is.EqualTo(expectedFirstResult));
             Assert.That(firstResult.State, Is.EqualTo(OutputState.Valid));
-            
+
             Assert.That(invalidResult.Output, Is.EqualTo(null));
             Assert.That(invalidResult.State, Is.EqualTo(OutputState.Continue));
-            
+
             Assert.That(secondResult.Output, Is.EqualTo(expectedSecondResult));
             Assert.That(secondResult.State, Is.EqualTo(OutputState.Valid));
         }
-        
+
         [Test]
-        public async Task Execute_Should_Continue_If_Not_Complete()
+        public async Task Execute_Should_Continue_When_Not_Complete()
         {
             // arrange
             var codeLines = new List<string>
@@ -93,24 +93,44 @@ namespace ReplTap.Core.Tests
                 "foo =\"bar\";",
                 "foo",
             };
-            
+
             var inputCheck = new Mock<IInputCheck>();
             var engine = new ReplEngine(inputCheck.Object);
             var builder = new StringBuilder();
             var codeResult = new CodeResult();
-            
-            // act 
-            
+
+            // act
+
             foreach (var line in codeLines)
             {
                 builder.AppendLine(line);
-                
+
                 codeResult = await engine.Execute(builder.ToString());
             }
-            
+
             // assert
             Assert.That(codeResult.Output, Is.EqualTo("bar"));
             Assert.That(codeResult.State, Is.EqualTo(OutputState.Valid));
-        } 
+        }
+
+        [Test]
+        public async Task Execute_Should_Return_Error_State_When_Invalid_Input_And_Force_Execute()
+        {
+            // arrange
+            var input = "invalid code with semicolon to force execute ; ";
+            var inputCheck = new Mock<IInputCheck>();
+            var engine = new ReplEngine(inputCheck.Object);
+
+            inputCheck
+                .Setup(i => i.IsForceExecute(input))
+                .Returns(true);
+
+            // act
+            var codeResult = await engine.Execute(input);
+
+            // assert
+            Assert.That(codeResult.Output, Is.EqualTo("(1,14): error CS1002: ; expected"));
+            Assert.That(codeResult.State, Is.EqualTo(OutputState.Error));
+        }
     }
 }
