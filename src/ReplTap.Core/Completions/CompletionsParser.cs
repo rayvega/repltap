4bcc66d81c22389a0515b3b6fa8995ff65768eba
Ray Vega <raymond.vega.0@gmail.com>
2019-11-17 10:29:28 -0800

    add parsing incomplete text from code

---
+++ src/ReplTap.Core/Completions/CompletionsParser.cs
@@ -0,0 +1,29 @@
+using System.Linq;
+using System.Text;
+
+namespace ReplTap.Core.Completions
+{
+    public class CompletionsParser
+    {
+        public string ParseIncompleteText(string code)
+        {
+            var builder = new StringBuilder(code.Length);
+
+            for (var index = code.Length - 1; index >= 0; index--)
+            {
+                var current = code[index];
+
+                if (!char.IsLetterOrDigit(current))
+                {
+                    break;
+                }
+
+                builder.Append(current);
+            }
+
+            var incompleteText = new string(builder.ToString().Reverse().ToArray());
+
+            return incompleteText;
+        }
+    }
+}
