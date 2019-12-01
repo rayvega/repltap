1883eeee5a0058fcb3951c93f9fe5c9c4680a959
Ray Vega <raymond.vega.0@gmail.com>
2019-12-01 00:10:49 -0800

    add test when key tab should call completions for console reader

--- test/ReplTap.ConsoleHost.Tests/ConsoleReaderTests.cs
+++ test/ReplTap.ConsoleHost.Tests/ConsoleReaderTests.cs
@@ -42,5 +42,43 @@ namespace ReplTap.ConsoleHost.Tests
             // assert
             Assert.That(input, Is.EqualTo("abc"));
         }
+
+        [Test]
+        public async Task ReadLine_Should_Return_Write_Completions_When_Key_Tab()
+        {
+            // arrange
+            var prompt = "test prompt *";
+
+            var console = new Mock<IConsole>();
+            var consoleKeys = new List<(char inputChar, ConsoleKey consoleKey)>
+            {
+                ('a', It.IsAny<ConsoleKey>()),
+                ('b', It.IsAny<ConsoleKey>()),
+                (It.IsAny<char>(), ConsoleKey.Tab),
+                ('d', ConsoleKey.Enter),
+            };
+
+            var setupSequence = console
+                .SetupSequence(c => c.ReadKey(true));
+
+            foreach (var (inputChar, consoleKey) in consoleKeys)
+            {
+                var consoleKeyInfo = new ConsoleKeyInfo(inputChar, consoleKey, false, false, false);
+
+                setupSequence.Returns(consoleKeyInfo);
+            }
+
+            var provider = new Mock<ICompletionsProvider>();
+            var consoleReader = new ConsoleReader(console.Object, provider.Object);
+
+            // act
+            var input = await consoleReader.ReadLine(prompt);
+
+            // assert
+            provider.Verify(p => p.GetCompletions("ab"), Times.Once());
+            console.Verify(c => c.WriteLine(It.IsAny<string>()), Times.AtLeastOnce());
+
+            Assert.That(input, Is.EqualTo("abd"));
+        }
     }
 }
