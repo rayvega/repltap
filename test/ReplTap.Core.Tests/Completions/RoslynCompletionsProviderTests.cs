dd559752f2fca5f2e2cc16085d5397e2a8a51f31
Ray Vega <raymond.vega.0@gmail.com>
2022-01-27 21:10:34 -0800

    refactor: format fixes & fix 'async void'

--- test/ReplTap.Core.Tests/Completions/RoslynCompletionsProviderTests.cs
+++ test/ReplTap.Core.Tests/Completions/RoslynCompletionsProviderTests.cs
@@ -9,15 +9,15 @@ namespace ReplTap.Core.Tests.Completions
     public class RoslynCompletionsProviderTests
     {
         [Test]
-        [TestCase("var foo = \"bar\"; foo.", new[] {"Contains", "EndsWith", "Substring", "ToLower", "Trim",})]
-        [TestCase("\"bar\".", new[] {"Contains", "EndsWith", "Substring", "ToLower", "Trim",})]
+        [TestCase("var foo = \"bar\"; foo.", new[] { "Contains", "EndsWith", "Substring", "ToLower", "Trim" })]
+        [TestCase("\"bar\".", new[] { "Contains", "EndsWith", "Substring", "ToLower", "Trim" })]
         public async Task GetCompletions_Should_Return_Expected_When_Dot_Completion(string code, string[] expectedCompletions)
         {
             // arrange
             var completionsProvider = new RoslynCompletionsProvider();
 
             // act
-            var completions = await completionsProvider.GetCompletions(code, new[] {"System"});
+            var completions = await completionsProvider.GetCompletions(code, new[] { "System" });
 
             // assert
             var actual = completions.Items.Select(i => i.DisplayText);
