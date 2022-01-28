85fc10595c33c3fcd83466af00ae55b015b886de
Ray Vega <raymond.vega.0@gmail.com>
2022-01-27 21:49:42 -0800

    refactor: change program main to use 'top level statements'

--- src/ReplTap/Program.cs
+++ src/ReplTap/Program.cs
@@ -1,34 +1,24 @@
 ﻿using System;
-using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using ReplTap;
 using ReplTap.ConsoleHost;
 
-namespace ReplTap
+if (Console.IsInputRedirected)
 {
-    public static class Program
-    {
-        public static async Task Main()
-        {
-            if (Console.IsInputRedirected)
-            {
-                Console.WriteLine("Error! Either input is redirected from standard input or app doesn't have console. Unable to run console app.");
+    Console.WriteLine("Error! Either input is redirected from standard input or app doesn't have console. Unable to run console app.");
 
-                return;
-            }
-
-            var version = typeof(Program)
-                .Assembly
-                .GetName()
-                .Version?
-                .ToString()[..^2];
-
-            Console.WriteLine($"repltap - C# interactive repl - v{version}");
-
-            var provider = Configuration.GetServiceProvider();
-            var interactiveLoop = provider.GetRequiredService<IInteractiveLoop>();
-
-
-            await interactiveLoop.Run();
-        }
-    }
+    return;
 }
+
+var version = typeof(Configuration)
+    .Assembly
+    .GetName()
+    .Version?
+    .ToString()[..^2];
+
+Console.WriteLine($"repltap - C# interactive repl - v{version}");
+
+var provider = Configuration.GetServiceProvider();
+var interactiveLoop = provider.GetRequiredService<IInteractiveLoop>();
+
+await interactiveLoop.Run();
